ce1c19b0b4174501937b5d17feef377cacfe1fed
fceylan <furkanceylan2211@gmail.com>
2023-09-07 13:48:42 +0300

    fix

--- uni-sim/controllers/placementController.js
+++ uni-sim/controllers/placementController.js
@@ -35,32 +35,6 @@ const addPlacementForAllStudents = async () => {
   }
 };
 
-const getStudentPlacements = async () => {
-  try {
-    const query = `
-      SELECT students.first_name, students.last_name, universities.name AS university_name, scores.score
-      FROM students
-      LEFT JOIN scores ON students.id = scores.student_id
-      LEFT JOIN universities ON students.university_id = universities.id;
-    `;
-    const result = await knex.raw(query);
-
-    const placements = result.rows.map((row) => ({
-      firstName: row.first_name,
-      lastName: row.last_name,
-      universityName: row.university_name,
-      score: row.score,
-    }));
-
-    placements.forEach((placement) => {
-      console.log(`${placement.firstName} ${placement.lastName} - ${placement.universityName}: ${placement.score}`);
-    });
-  } catch (error) {
-    console.error('Error fetching student placements:', error);
-  }
-};
-
 module.exports = {
   addPlacementForAllStudents,
-  getStudentPlacements,
 };
